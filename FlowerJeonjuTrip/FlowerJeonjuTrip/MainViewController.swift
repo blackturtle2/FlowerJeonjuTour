47b041a7569be8c8658c04958672842a7549a45b
leejaesung <blackturtle2@gmail.com>
2017-11-13 15:10:02 +0900

    [Create] Xcode Project Init

---
+++ FlowerJeonjuTrip/FlowerJeonjuTrip/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlowerJeonjuTrip
+//
+//  Created by leejaesung on 2017. 11. 13..
+//  Copyright © 2017년 leejaesung. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
