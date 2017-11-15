026b6a4df10444f9b0495f85f27b76d4dad1eb70
leejaesung <blackturtle2@gmail.com>
2017-11-16 02:26:46 +0900

    [Update] 마이뱃지 뷰 UI 구성중

---
+++ FlowerJeonjuTrip/FlowerJeonjuTrip/MyBadgeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyBadgeCollectionViewCell.swift
+//  FlowerJeonjuTrip
+//
+//  Created by leejaesung on 2017. 11. 16..
+//  Copyright © 2017년 leejaesung. All rights reserved.
+//
+
+import UIKit
+
+class MyBadgeCollectionViewCell: UICollectionViewCell {
+    
+}
